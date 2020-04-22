d9f18ec492376e2b4f1745fb766d17233ae876f2
alexandercortez <joelalexander10@outlook.com>
2020-04-21 22:01:27 -0600

    Milestone 1: production

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+const path = require('path');
+const express = require('express');
+
 const app = require('./config/express');
+
+app.use(express.static(path.join(__dirname, 'client')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/client/index.html`));
+});
 
 const { PORT } = process.env;
 
---
+++ client/precache-manifest.7268b1b4f3112c77b21d8997cac7e6ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b500c84fd80323cbd0a422edc30a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "797fab2721b0d5e7ee10",
+    "url": "/static/css/main.a293d3ea.chunk.css"
+  },
+  {
+    "revision": "663d3600b20b7c66b839",
+    "url": "/static/js/2.39ef756c.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.39ef756c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797fab2721b0d5e7ee10",
+    "url": "/static/js/main.8d7817dc.chunk.js"
+  },
+  {
+    "revision": "96b8e36beec96a36135e",
+    "url": "/static/js/runtime-main.569e9676.js"
+  }
+]);
