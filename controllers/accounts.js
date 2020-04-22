65423e630c8ad7bc24a8bcbdc8df657a5b3663a3
alexandercortez <joelalexander10@outlook.com>
2020-04-22 15:33:32 -0600

    Milestone 3: Manage messages per voicemail box

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -1,9 +1,9 @@
 const config = require('../config');
 
 const getMessages = (req, res) => {
-  const vmboxIdParam = config.vmBoxId;
+  const { vmBoxId } = req.params;
   superagent
-    .get(`/accounts/${config.accountId}/vmboxes/${vmboxIdParam}/messages`)
+    .get(`/accounts/${config.accountId}/vmboxes/${vmBoxId}/messages`)
     .set('Authorization', `Basic ${config.credentials}`)
     .then((response) => {
       res.sendJSONSuccess(response.body);
@@ -14,17 +14,28 @@ const getMessages = (req, res) => {
 };
 
 const changeStatus = (req, res) => {
-  const { messageId } = req.params;
+  const { messageId, vmBoxId } = req.params;
   const { status } = req.body;
   const validStatus = ['new', 'deleted', 'saved'];
   if (!status || !validStatus.includes(status)) {
     return res.sendJSONInvalid('Please, provide a valid status');
   }
-  const vmboxIdParam = config.vmBoxId;
   return superagent
-    .post(`/accounts/${config.accountId}/vmboxes/${vmboxIdParam}/messages/${messageId}`)
+    .post(`/accounts/${config.accountId}/vmboxes/${vmBoxId}/messages/${messageId}`)
     .set('Authorization', `Basic ${config.credentials}`)
     .send({ data: { folder: status } })
+    .then((response) => {
+      res.sendJSONSuccess(response.body);
+    })
+    .catch((err) => {
+      res.sendJSONError(err);
+    });
+};
+
+const getVmBoxes = (req, res) => {
+  superagent
+    .get(`/accounts/${config.accountId}/vmboxes`)
+    .set('Authorization', `Basic ${config.credentials}`)
     .then((response) => {
       res.sendJSONSuccess(response.body);
     })
@@ -36,4 +47,5 @@ const changeStatus = (req, res) => {
 module.exports = {
   getByVmBoxId: getMessages,
   changeStatus,
+  getVmBoxes,
 };
